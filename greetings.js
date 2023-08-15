bfab90f47dfa560cdcb010d273f5ba424bb99988
pankajajoshi <joshi.pankaja@gmail.com>
2023-08-14 22:26:43 -0400

    minor changes in file

--- greetings.js
+++ greetings.js
@@ -1,11 +1,10 @@
 
-
 const express = require('express');
 const router = express.Router();
 
 router.get('/:name', (req, res) => {
   const name = req.params.name || 'User';
-  res.send(`Wow! Hello there, ${name}!`);
+  res.send(`<h1>Wow! Hello there, ${name}!</h1>`);
 });
 
 module.exports = router;
